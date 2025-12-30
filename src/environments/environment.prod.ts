baf1f5d97c8d9fde64b6160ed8c2a86a1d32666a
HarisKulas91 <38757076+HarisKulas91@users.noreply.github.com>
2025-12-30 16:01:29 +0100

    update scope

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   entraId: {
     tenantId: '93307a6e-8ace-43b0-82cb-808486fdd761', // Tvoj Tenant ID
     clientId: 'fcc16c18-a4ec-47ac-8fe0-7744c442e2f7', // Tvoj Client ID
-    scope: 'User.Read', // Microsoft Graph scope for reading user profile
+    scope: 'api://fcc16c18-a4ec-47ac-8fe0-7744c442e2f7/access_as_user', // Microsoft Graph scope for reading user profile
   },
 };
 
