c5bdf4c414ddf7db8edabe1c96458718a9527ddb
HarisKulas91 <38757076+HarisKulas91@users.noreply.github.com>
2025-12-30 14:36:58 +0100

    update env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://api.example.com',
-  aiApiBaseUrl: 'https://api.example.com',
+  apiBaseUrl: 'https://hmi-backend.azurewebsites.net',
+  aiApiBaseUrl: 'https://tke-agentic-ai.azurewebsites.net',
   entraId: {
     tenantId: '93307a6e-8ace-43b0-82cb-808486fdd761', // Tvoj Tenant ID
     clientId: 'fcc16c18-a4ec-47ac-8fe0-7744c442e2f7', // Tvoj Client ID
